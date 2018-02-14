d5204eaeb88fa46a7cfa78107942c48faef4e58e
杨国梁 <guoliang.yang@huilianyi.com>
2018-02-14 16:15:29 +0800

    项目初始化提交-后台管理模块

---
+++ wiseautom-shiro/src/main/java/com/wiseautom/dao/SysOssDao.java
@@ -0,0 +1,14 @@
+package com.wiseautom.dao;
+
+import com.wiseautom.entity.SysOss;
+
+/**
+ * oss配置
+ * 
+ * @author chenyi
+ * @email 228112142@qq.com
+ * @date 2017-12-13 10:07:04
+ */
+public interface SysOssDao extends BaseDao<SysOss> {
+	
+}
